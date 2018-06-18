b6db687b94006c9c5bc209b50e6bf228176f0ea9
a-jie <a-jie.cn@msn.com>
2018-06-18 09:31:46 +0800

    replace js file
    replace js file

--- source/script.js
+++ source/script.js
@@ -1,15 +1,24 @@
 var map = require('map-stream');
 var vfs = require('vinyl-fs');
 
-var replace = function (file, cb) {
-    var content = file.contents;
-    content = content.toString('utf8');
-    content = content.replace('/banner.jpg','../banner.jpg').replace('/font/','../font/')
-    file.contents = Buffer.from(content);
-    
-    cb(null, file);
-};
-
 vfs.src(['./dist/css/*.css'])
-    .pipe(map(replace))
+    .pipe(map(function (file, cb) {
+        var content = file.contents;
+        content = content.toString('utf8');
+        content = content.replace('/banner.jpg', '../banner.jpg').replace('/font/', '../font/')
+        file.contents = Buffer.from(content);
+
+        cb(null, file);
+    }))
     .pipe(vfs.dest('./dist/css/'));
+
+vfs.src(['./dist/js/*.js'])
+    .pipe(map(function (file, cb) {
+        var content = file.contents;
+        content = content.toString('utf8');
+        content = content.replace('/data.yaml', '../data.yaml');
+        file.contents = Buffer.from(content);
+
+        cb(null, file);
+    }))
+    .pipe(vfs.dest('./dist/js/'));
