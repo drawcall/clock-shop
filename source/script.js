163d2e65c6bcd641b289f55cc0e7792f195ed0cb
ajiemath <ajiemath@gmail.com>
2019-12-30 12:02:11 +0800

    Upgrade version and modify multiple configurations

--- source/script.js
+++ source/script.js
@@ -1,24 +1,30 @@
-var map = require('map-stream');
-var vfs = require('vinyl-fs');
+var map = require("map-stream");
+var vfs = require("vinyl-fs");
 
-vfs.src(['./dist/css/*.css'])
-    .pipe(map(function (file, cb) {
-        var content = file.contents;
-        content = content.toString('utf8');
-        content = content.replace('/banner.jpg', '../banner.jpg').replace(/\/font\//ig, '../font/')
-        file.contents = Buffer.from(content);
+vfs
+  .src(["./dist/css/*.css"])
+  .pipe(
+    map(function(file, cb) {
+      var content = file.contents;
+      content = content.toString("utf8");
+      content = content.replace("/banner.jpg", "../banner.jpg").replace(/\/font\//gi, "../font/");
+      file.contents = Buffer.from(content);
 
-        cb(null, file);
-    }))
-    .pipe(vfs.dest('./dist/css/'));
+      cb(null, file);
+    })
+  )
+  .pipe(vfs.dest("./dist/css/"));
 
-vfs.src(['./dist/js/*.js'])
-    .pipe(map(function (file, cb) {
-        var content = file.contents;
-        content = content.toString('utf8');
-        content = content.replace('/data.yaml', './data.yaml');
-        file.contents = Buffer.from(content);
+vfs
+  .src(["./dist/js/*.js"])
+  .pipe(
+    map(function(file, cb) {
+      var content = file.contents;
+      content = content.toString("utf8");
+      content = content.replace("/data.yaml", "./data.yaml");
+      file.contents = Buffer.from(content);
 
-        cb(null, file);
-    }))
-    .pipe(vfs.dest('./dist/js/'));
+      cb(null, file);
+    })
+  )
+  .pipe(vfs.dest("./dist/js/"));
